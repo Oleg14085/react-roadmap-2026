981ad7bb75e6bba336001ff1e65e6d36e77b3886
Oleg <OLeg14085@mail.ru>
2026-05-16 15:30:47 +0300

    feat(w1-d1): closures - counter & memoizer with Map

---
+++ week-01/day-01-closures/index.js
@@ -0,0 +1,39 @@
+function createCounter(initial = 0) {
+  let count = initial;
+  return {
+    increment() {
+      return count++;
+    },
+    decrement() {
+      return count--;
+    },
+    reset() {
+      return (count = 0);
+    },
+    getValue() {
+      return count;
+    },
+  };
+}
+let counter1 = createCounter();
+let counter2 = createCounter();
+counter1.increment();
+counter2.decrement();
+console.log(counter1.getValue());
+console.log(counter2.getValue());
+
+function createMemoizer(fn) {
+ const cash = new Mam();
+ return function fn(arg){
+  if(cash.has(arg)){
+    console.log('♻️ Cache hit')
+    return cash
+  }else{
+    const result = fn(arg);
+    cash.set(arg,result);
+    console.log('🆕 Calculated');
+    return result
+  }
+ }
+  
+}
