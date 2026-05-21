3695942dc18ad9b86993cf4926f880b40fbda08a
Oleg <OLeg14085@mail.ru>
2026-05-21 15:27:31 +0300

    feat(w1-d2): event loop & promise.all implementation

---
+++ week-01/day-02-event-loop/index.js
@@ -0,0 +1,32 @@
+function runAsyncRace() {
+  console.log("1. Sync"); //сначала синхронный код
+  setTimeout(() => console.log("2. setTimeout"), 0); // После всех микротасков и перерисовки экрана
+  Promise.resolve().then(() => console.log("3. Promise")); // Сразу после текущего синхронного кода, до рендеринга
+  console.log("4. Sync end"); //сначала синхронный код
+}
+runAsyncRace();
+
+function simplePromiseAll(promises){
+  return new Promise((resolve,reject)=>{
+    const result = new Array(promises.length)
+    let resolvedCount = 0
+    promises.forEach((promise,index)=>{
+      promise
+      .then((value)=>{
+        result[index]=value
+        resolvedCount++
+        if(promises.length===resolvedCount){
+          resolve(result)
+      }
+      })
+      .catch((error)=>reject(error))
+    })
+  })
+}
+simplePromiseAll([
+  new Promise((res) => setTimeout(() => res("A"), 100)),
+  new Promise((res) => setTimeout(() => res("B"), 50)),
+  new Promise((_, rej) => setTimeout(() => rej("❌ Fail"), 20)),
+])
+  .then(console.log)
+  .catch(console.error);
