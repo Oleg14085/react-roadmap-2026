9dd950f0e38acec8dbffd833d125050e4c09ed47
Oleg <OLeg14085@mail.ru>
2026-07-10 15:03:34 +0300

    practice with operators ts

---
+++ week-02/day-01-ts-practice/r5-practiceWithOperators.ts
@@ -0,0 +1,27 @@
+export {}
+type ThemeColor = string | [number, number, number];
+type ThemeConfig = Record<string, ThemeColor>;
+const appTheme = {
+    primery:'#ff0000',
+    secondary:[0,255,0]
+} satisfies ThemeConfig;
+appTheme.primery.toUpperCase();
+appTheme.secondary.push(128);
+
+
+type TextNotification = { id: number; text: string };
+type ImageNotification = { id: number; imageUrl: string };
+function renderNotification(notif: TextNotification | ImageNotification){
+    if("text"in notif){
+       console.log(`Текст: [значение ${notif.text}]`)
+    }
+    else{
+        console.log(`Картинка: [значение ${notif.imageUrl}]`)
+    }
+}
+
+
+const rawServerData:unknown = { name: "Админ", role: "superuser" };
+interface User{name:string, role:string }
+const rawServerData1 = rawServerData as User 
+console.log(`Привет, [${rawServerData1.name}], твоя роль: [${rawServerData1.role}]`)
